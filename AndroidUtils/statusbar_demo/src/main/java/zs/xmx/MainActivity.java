8b93bce2c1fd3346f5f7b21bc85f183d6f38dc05
moxiaoming <maiqiming487@qq.com>
2017-07-07 17:29:27 +0800

    创建status_demo(未开始)

---
+++ AndroidUtils/statusbar_demo/src/main/java/zs/xmx/MainActivity.java
@@ -0,0 +1,13 @@
+package zs.xmx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//TODO 博客的三种方案这里写一遍,重新更新博客,以及重新排版
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
